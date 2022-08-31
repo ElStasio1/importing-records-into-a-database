6dce8ce5544ba5083da885e861be56ec75d88873
ElStasio1 <112546377+ElStasio1@users.noreply.github.com>
2022-08-31 16:09:05 +0600

    Add files via upload

---
+++ functions/script.php
@@ -0,0 +1,32 @@
+<?php
+require 'include/db.php';
+$post_count = 0;
+$comment_count = 0;
+
+
+$jsonStringPosts = file_get_contents('https://jsonplaceholder.typicode.com/posts');
+$jsonArrayPosts = json_decode($jsonStringPosts, true);
+
+$jsonStringComments = file_get_contents('https://jsonplaceholder.typicode.com/comments');
+$jsonArrayComments = json_decode($jsonStringComments, true);
+
+foreach ($jsonArrayPosts as $item) 
+{	
+	$stmt = $mysql -> prepare("INSERT INTO post (userId, id, title, body) VALUES (?, ?, ?, ?)");
+	$stmt -> bind_param("iiss", $item['userId'], $item['id'], $item['title'], $item['body']);	
+	$stmt -> execute();
+	$post_count +=1;
+}
+
+foreach ($jsonArrayComments as $item) 
+{	
+	$stmt = $mysql -> prepare("INSERT INTO comment (postId, id, name, email, body) VALUES (?, ?, ?, ?, ?)");
+	$stmt -> bind_param("iisss", $item['postId'], $item['id'], $item['name'], $item['email'], $item['body']);	
+	$stmt -> execute();
+	$comment_count +=1;
+}
+
+$mysql -> close();
+
+echo "Загружено {$post_count} записей и {$comment_count} комментариев";
+?>
--- index.php
+++ index.php
@@ -1,44 +1,75 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Поиск</title>
-<meta charset="UTF-8">
-<style>
-table, th, td {border: 1px solid black;border-collapse: collapse;}
-</style>
+	<title>Поиск</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.css">
 </head>
+
 <body>
-<form action="" method="GET">
-        <input type="text" name="query" />
-		<input type="submit" name="submit" value="Найти" />
-</form>
-<?php
-require 'db.php';
+	<div class="container">
+		<h1>Поиск</h1>
+		<form action="" method="GET">
+			<input class="form-control mb-2" type="text" name="query" placeholder="Поиск">
+			<input class="btn btn-success" type="submit" name="submit" value="Найти">
+		</form>
+		<?php
+		require 'include/db.php';
 
-if(isset($_GET['query'])) {
-	$query = $_GET['query'];
-	if(strlen($query) >= 3){
-		$stmt = $mysql -> prepare("SELECT post.title, comment.body FROM post INNER JOIN comment ON comment.postid = post.id WHERE comment.body LIKE ?");
-		$query = "%" . $query . "%";
-		$stmt -> bind_param("s", $query);
-		$stmt -> execute();
-		$arr = $stmt -> get_result()->fetch_all(MYSQLI_ASSOC);
-		$stmt -> close(); }
-	else {
-		echo "Min search length is 3 characters | Минимальная длинна запроса 3 символа"; }
+		if (isset($_GET['query'])) {
+			$query = $_GET['query'];
+			if (strlen($query) >= 3) {
+				$stmt = $mysql->prepare("SELECT post.title, comment.body FROM post INNER JOIN comment ON comment.postid = post.id WHERE comment.body LIKE ?");
+				$query = "%" . $query . "%";
+				$stmt->bind_param("s", $query);
+				$stmt->execute();
+				$search = $stmt->get_result();
+				$search_result = mysqli_num_rows($search); ?>
+				<?php
+				if ($search_result == 0) { ?>
+					<h4>По запросу ничего не найдено</h4>
 
-if (isset($arr)){
-	if (count($arr) > 0){
-		echo "<h2>Результаты Поиска</h2>";
-		echo "<table style=\"width:80%\"><tr><th>Запись</th><th>Комментарий</th></tr>";
-		foreach ($arr as $item) {
-			echo("<tr><td>{$item['title']}</td><td>{$item['body']}</td></tr>"); }
-} 	else {
-		echo "Результаты не найдены";}
+				<?php } else { ?>
+					<table class="table">
+						<thead>
+							<tr>
+								<h1 class="text-center">Статьи</h1>
 
-echo "</table>";
-}
-}
-?>
+								<td>Запись</td>
+								<td>Комментарий</td>
+							</tr>
+						</thead>
+						<tbody>
+							<?php while (($search_result = $search->fetch_assoc()) ?? null) {
+							?>
+
+								<tr>
+									<td><?= $search_result['title'] ?></td>
+									<td><?= $search_result['body'] ?></td>
+								</tr>
+
+							<?php } ?>
+						</tbody>
+					</table>
+
+
+				<?php } ?>
+
+		<?php
+
+
+
+			} else {
+				echo "Минимальная длинна запроса 3 символа";
+			}
+		}
+		?>
+
+
+
+	</div>
+
 </body>
+
 </html>
